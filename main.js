e9966298200913d5059f20892fee871ec10e72d7
NomadRazor <nmdrzr@yandex.ru>
2018-09-17 02:43:27 +0300

    release

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+let template = [];
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 function createWindow () {
   // Create the browser window.
